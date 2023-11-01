0c9c14e163b05f2940a36a6b0acb9c5bed2432a8
Olga <107925095+CatIsOutOfTheBag@users.noreply.github.com>
2023-11-01 10:50:21 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,29 @@
-from flask import Flask
-import sys
-sys.path.append('src')
+from flask import Flask,request
+from flask_restful import Resource, Api
+import pickle
+import pandas as pd
+import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+#
+CORS(app)
+# creating an API object
+api = Api(app)
 
-@app.route("/")
-def index():
-    return "Hello!"
+#prediction api call
+class prediction(Resource):
+    def get(self,number):
 
-if __name__=="__main__":
-    app.run()
+        row_df = pd.DataFrame.from_dict({'tx_amount': [number+random.randint(0,10)],
+                        'tx_time_seconds': [number+random.randint(0,10)],
+                        'tx_time_days': [number+random.randint(0,10)]})        
+                
+        model = pickle.load(open('dump.pkl', 'rb'))
+        prediction = model.predict(row_df)       
+        return str(prediction)
+
+api.add_resource(prediction, '/prediction/<int:number>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
